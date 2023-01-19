df26018e35120cd3b73fd542ffc8dde78b4e34e5
Tiui <thiago.steinberg@hotmail.com>
2023-01-19 00:11:17 -0300

    Operações CRUD nos utils de servidor

--- server/api/collectionTest.ts
+++ server/api/collectionTest.ts
@@ -1,11 +1,10 @@
-import { fetchData } from '../utils/firestore'
+import { getDocFromCollection, getDocsFromCollection, deleteDocFromCollection } from './../utils/firestore';
 
 export default defineEventHandler(async (event) => {
-    // return { user: event.context.user } retorna um JSON
 
-    let data = await fetchData('users/P8V73vUNlIZuhDHuSbCS/characters')
 
-    // Retorna um objeto :)
-    return { data: data } 
-    // event.node.res.end(data) 
+
+
+
+    
 })
