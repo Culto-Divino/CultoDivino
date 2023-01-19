8632f0f1804e5c787294b45e5d26804dfa4cacfd
Tiui <thiago.steinberg@hotmail.com>
2023-01-19 03:35:27 -0300

    Remanejamento de estrutura de pastas, atualização de objetos de usuário

---
+++ server/api/userStateChanged.post.ts
@@ -0,0 +1,20 @@
+import { setDocFromCollection } from './../utils/firestore';
+import { addDocToCollection } from '../utils/firestore';
+import { serverTimestamp } from 'firebase/firestore';
+
+export default defineEventHandler(async (event) => {
+
+    const body = await readBody(event)
+    const user = body.user
+
+    if(!body.user)
+      return { success: false}
+
+    
+    const userDoc = {
+      lastSignedIn: serverTimestamp()
+    }
+    const userRef = await setDocFromCollection(userDoc, user.uid, 'users', true)
+
+    return { success: true }
+})
