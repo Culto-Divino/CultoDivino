cb5719ca13cb254316a3bb6127b95f596546bb26
guilhermesantos0 <guilhermesantosdev@outlook.com>
2023-01-22 01:19:11 -0300

    Anotações - Começando

---
+++ pages/[characterId]/anotacoes.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="w-screen h-screen bg-gradient-to-r from-bgColor1 to-bgColor2 overflow-y-scroll overflow-x-hidden z-0">
+        <CharacterHeader class="sticky top-0 z-10" />
+        <div class="w-full h-full flex items-center justify-center">
+            <div class="w-11/12 h-5/6 pt-2 overflow-y-scroll overflow-x-hidden bg-gray-400/50 flex flex-col items-center">
+                <Note 
+                title="Título"
+                note="Anotaçãojksadnsakjdjskadnjkasdjksanksakjdshbakjdhsajdjasahjsdbsahjdbsahj"
+                />
+                <Note 
+                title="Título"
+                note="Anotaçãojksadnsakjdjskadnjkasdjksanksakjdshbakjdhsajdjasahjsdbsahjdbsahj"
+                />
+                <Note 
+                title="Título"
+                note="Anotaçãojksadnsakjdjskadnjkasdjksanksakjdshbakjdhsajdjasahjsdbsahjdbsahj"
+                />
+                <Note 
+                title="Título"
+                note="Anotaçãojksadnsakjdjskadnjkasdjksanksakjdshbakjdhsajdjasahjsdbsahjdbsahj"
+                />
+                <Note 
+                title="Título"
+                note="Anotaçãojksadnsakjdjskadnjkasdjksanksakjdshbakjdhsajdjasahjsdbsahjdbsahj"
+                />
+            </div>
+        </div>
+    </div>
+</template>
--- pages/[characterId]/combate.vue
+++ pages/[characterId]/combate.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="w-screen h-screen bg-gradient-to-r from-bgColor1 to-bgColor2 overflow-y-scroll overflow-x-hidden z-0">
+    <div id="combatContainer" class="w-screen h-screen bg-gradient-to-r from-bgColor1 to-bgColor2 overflow-y-scroll overflow-x-hidden z-0">
         <CharacterHeader class="sticky top-0 z-10" />
+
+        <Modal 
+        title="Digite aqui o seu estado:"
+        />
+
         <div class="h-2/4 flex flex-col text-white items-center mt-10">
             <div class="flex w-9/12 h-4/6 flex items-center justify-center text-2xl">
                 <CombatData 
@@ -31,7 +36,7 @@
             <h1 class="text-3xl text-white">Estados</h1>
             <div class="w-10/12 h-16 mt-2 bg-gradient-to-r from-gray-200/25 to-gray-500/50 rounded-md flex justify-between items-center">
                 <div class="h-5/6 w-10/12 ml-2 truncate text-white flex items-center text-2xl">Morrendo, Congelado</div>
-                <button class="h-12 w-12 mr-2"><p class="flex items-center justify-center text-6xl rounded-full bg-white h-full">+</p></button>
+                <button @click="addState()" class="h-12 w-12 mr-2"><p class="flex items-center justify-center text-6xl rounded-full bg-white h-full">+</p></button>
             </div>
         </div>
         <div class="h-2/4 flex flex-col text-white items-center mt-10">
@@ -61,6 +66,12 @@
             <button class="w-5/12 h-16 flex items-center justify-center"><img src="../../images/dice.png" class="w-5/12 h-16 mr-5" alt=""></button>
         </div>
     </div>
+
 </template>
+
+<script setup>
+
+
+</script>
 
 <!-- <font-awesome-icon class="w-5/12 h-16 text-6xl text-white" icon="fa-solid fa-dice-five" /> -->
