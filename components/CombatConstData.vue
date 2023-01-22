b9d1edc09887988c62a48e58d46a06d18d7a6110
guilhermesantos0 <guilhermesantosdev@outlook.com>
2023-01-21 21:09:38 -0300

    combat menu

--- components/CombatConstData.vue
+++ components/CombatConstData.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col items-center justify-center w-5/12 h-3/6 mr-2 rounded-2xl border border-slate-600 shadow-md shadow-cyan-500 active:translate-y-px">
         <h2>{{ title }}</h2>
-        <input type="text" class="bg-transparent w-10 outline-none text-center" :value="value">
+        <p class="bg-transparent w-10 outline-none text-center">{{ value }}</p>
     </div>
 </template>
 
