c7f1d0c0ad16efa08bdddb96f28a596b127e4d97
guilhermesantos0 <guilhermesantosdev@outlook.com>
2023-01-21 21:09:38 -0300

    combat menu

---
+++ components/CombatConstData.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex flex-col items-center justify-center w-5/12 h-3/6 mr-2 rounded-2xl border border-slate-600 shadow-md shadow-cyan-500 active:translate-y-px">
+        <h2>{{ title }}</h2>
+        <p class="bg-transparent w-10 outline-none text-center">{{ value }}</p>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    title: String,
+    value: String
+})
+</script>
---
+++ components/CombatData.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-col items-center justify-center w-5/12 h-3/6 mr-1 rounded-2xl border border-slate-600 shadow-md shadow-cyan-500 active:translate-y-px">
+        <h2>{{ title }}</h2>
+        <p>{{ value }}/{{ maxValue }}</p>
+    </div>
+</template>
+
+<script setup>
+
+    const props = defineProps({
+        title: String,
+        value: String,
+        maxValue: String
+    })
+
+</script>
