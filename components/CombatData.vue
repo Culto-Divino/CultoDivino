4bff0d47a0dfe6ff525f0b99ca577a09f5013395
guilhermesantos0 <guilhermesantosdev@outlook.com>
2023-01-21 23:47:14 -0300

    Fix: input value on combat data

--- components/CombatData.vue
+++ components/CombatData.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col items-center justify-center w-5/12 h-3/6 mr-1 rounded-2xl border border-slate-600 shadow-md shadow-cyan-500 active:translate-y-px">
         <h2>{{ title }}</h2>
-        <p>{{ value }}/{{ maxValue }}</p>
+        <p><input type="text" :value="value" class="bg-transparent w-10 outline-none text-right">/{{ maxValue }}</p>
     </div>
 </template>
 
