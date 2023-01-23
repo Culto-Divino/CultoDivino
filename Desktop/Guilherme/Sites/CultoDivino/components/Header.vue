086aa8d0fe0c90aadf40ca7ae7371647b83a5ac2
Tiui50 <60558335+Tiui50@users.noreply.github.com>
2023-01-22 23:31:55 -0300

    Delete Desktop/Guilherme/Sites/CultoDivino directory

--- Desktop/Guilherme/Sites/CultoDivino/components/Header.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="absolute top-0 w-screen h-16 bg-white/75 flex flex-col items-center justify-center text-center">
-        <h1 class="text-3xl w-6/12">O Culto Divino</h1>
-    </div>
-</template>
--- Desktop/Guilherme/Sites/CultoDivino/pages/[[characterId]]/inventario.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        Inventario
-    </div>
-</template>
