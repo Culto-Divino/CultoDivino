95a0bb4e9b576ca24ea37cc152e8645a0f162d84
guilhermesantos0 <guilhermesantosdev@outlook.com>
2023-01-25 01:21:51 -0300

    loading component

---
+++ pages/loading-DEV.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-screen h-screen bg-gradient-to-r from-bgColor1 to-bgColor2 flex flex-col justify-center items-center truncate text-white">
+        <Loading />
+    </div>
+</template>
